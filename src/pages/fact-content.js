b8eaca05c591c4e217b584bff3047c454987b7c1
Andrew Koo <andrewkoo@Andrews-MBP-16.lan>
2023-02-06 11:27:58 -0500

    create mock facts data

---
+++ src/pages/fact-content.js
@@ -0,0 +1,52 @@
+const facts = [
+  {
+    name: 'user1',
+    title:
+      'Billionaire Charles Koch-Backed Group Will Push GOP to Move Past Donald Trump',
+    content: [
+      `WASHINGTON—A group funded by billionaire Charles Koch will work to support a Republican presidential nominee other than Donald Trump, concluding in a strategy memo that “we need to turn the page on the past.”
+      
+      The organization, Americans for Prosperity, has stayed out of the last two presidential cycles but has concluded it needs to engage now as Mr. Trump mounts his third consecutive White House run. The memo released Sunday doesn’t mention the former president by name but is unambiguous in its purpose.
+      
+      “To write a new chapter for our country, we need to turn the page on the past,” the document reads. “So the best thing for the country would be to have a president in 2025 who represents a new chapter. The American people have shown that they’re ready to move on, and so AFP will help them do that.”`,
+    ],
+  },
+  {
+    name: 'user2',
+    title:
+      'Adani cut its UK audit bill in half by dumping a Big 4 firm for a much smaller rival, filings show as scrutiny of its safeguards deepens',
+    content: [
+      `In this article, we're going to be talking looking at a very quick way
+            to set up a Node.js server. We'll be discussing some topics such as proin congue
+            ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
+            Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
+            non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
+            eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
+            sodales purus euismod.`,
+      `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+      `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+    ],
+  },
+  {
+    name: 'user3',
+    title:
+      'Arctic blast sweeps through US north-east with record-breaking temperatures',
+    content: [
+      `An Arctic blast has swept through numerous parts of the north-east, leaving millions of people under wind chill warnings and cities and states issuing emergency warnings.
+      
+      Record-breaking temperatures will be 10 to 30 degrees below average over parts of the north-east and into the coastal mid-Atlantic, the National Weather Service announced on Saturday, with wind gusts potentially reaching up to 40 to 55mph (64 to 88km/h).`,
+    ],
+  },
+];
+
+export default facts;
