ffbd1cea6a8bc9b210dbf5a5732c2330ea851fae
Andrew Koo <andrewkoo@Andrews-MBP-16.lan>
2023-02-06 16:02:16 -0500

    create factslist component that can be reused for different lists

--- src/pages/FactsListPage.js
+++ src/pages/FactsListPage.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import facts from './fact-content';
-import { Link } from 'react-router-dom';
+
+import FactsList from '../components/FactsList';
 
 const FactsListPage = () => {
   return (
-    <div>
+    <>
       <h1>Facts</h1>
       <br />
-      {facts.map((fact) => (
-        <div key={fact.id}>
-          <Link
-            className='fact-list-item'
-            to={`/facts/${fact.username}&${fact.number}`}
-            key={fact.id}
-          >
-            <h3>{fact.title}</h3>
-            <p>{fact.content[0].substring(0, 150)}...</p>
-            <br />
-          </Link>
-        </div>
-      ))}
-    </div>
+      <FactsList facts={facts} />
+    </>
   );
 };
 
