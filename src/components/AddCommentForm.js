04a7fd120c594b062de453fdaf9f1c86ca821387
Andrew Koo <andrewkoo@Andrews-MBP-16.lan>
2023-02-11 23:58:52 -0500

    add user token to all axios requests

--- src/components/AddCommentForm.js
+++ src/components/AddCommentForm.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import useUser from '../hooks/useUser';
 
 const AddCommentForm = ({ title, onFactUpdated }) => {
   const [name, setName] = useState('');
   const [commentText, setCommentText] = useState('');
 
+  const { user, isLoading } = useUser();
+
   const addComment = async () => {
-    const response = await axios.post(`/api/facts/${title}/comments`, {
-      postedBy: name,
-      text: commentText,
-    });
+    const token = user && (await user.getIdToken());
+    const headers = token ? { authtoken: token } : {};
+    const response = await axios.post(
+      `/api/facts/${title}/comments`,
+      {
+        postedBy: name,
+        text: commentText,
+      },
+      { headers }
+    );
 
     const updatedFact = response.data;
 
